65da17b61be3b09e5964d4ae8140917e6e918c29
chunfu <hideoryan@gmail.com>
2025-02-28 00:19:57 +0800

    feat: play the real video

--- src/Page2.tsx
+++ src/Page2.tsx
@@ -1,25 +1,7 @@
 import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
-/*
-import video1 from "./assets/1.mp4";
-import video2 from "./assets/2.mp4";
-import cam1 from "./assets/cam1.png";
-import cam2 from "./assets/cam2.png";
-import cam3 from "./assets/cam3.png";
-import cam4 from "./assets/cam4.png";
-*/
-import cam5 from "./assets/cam5.png";
+import video5 from "./assets/Live+.mov";
 import Button from "./Button";
-
-/*
-const videos = [
-  { id: 1, description: "視角 1", src: cam1 },
-  { id: 2, description: "視角 2", src: cam2 },
-  { id: 3, description: "視角 3", src: cam3 },
-  { id: 4, description: "視角 4", src: cam4 },
-  { id: 5, description: "視角 5", src: cam5 },
-];
-*/
 
 const content = `
 請先觀賞下方的一分鐘影片。
@@ -36,8 +18,7 @@ const Page2: React.FC<PageProps> = ({ onClick }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsButtonDisabled(false);
-      // }, 60000); // 60 seconds
-    }, 5000); // 5 seconds
+    }, 60000); // 60 seconds
 
     return () => clearTimeout(timer);
   }, []);
@@ -47,11 +28,11 @@ const Page2: React.FC<PageProps> = ({ onClick }) => {
       <div className="prose">
         <Markdown>{content}</Markdown>
       </div>
-      <img
+      <video
         className="w-full h-1/3 my-5 object-contain object-center rounded"
-        src={cam5}
-        // autoPlay
-        // playsInline
+        src={video5}
+        autoPlay
+        playsInline
       />
       <Button onClick={onClick} disabled={isButtonDisabled}>
         繼續填答
--- src/Page3.tsx
+++ src/Page3.tsx
@@ -4,7 +4,7 @@ const Page1: React.FC = () => {
   return (
       <iframe
         sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-modals"
-        src="https://www.surveycake.com/s/323yN"
+        src="https://www.surveycake.com/s/lmZk1"
         allowTransparency={true}
         className="w-full h-[100vh] border border-[#ddd]"
       >
